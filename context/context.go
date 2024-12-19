29da868ad8941b5d04baae6f1a2366211d03063f
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-19 13:08:34 -0400

    feat: Added GetClientIP function
    * Added GetClientIP function
    * Forked from 'github.com/pixel-plaza-dev/uru-databases-2-go-service-common'

---
+++ context/context.go
@@ -0,0 +1,28 @@
+package context
+
+import (
+	"context"
+	"google.golang.org/grpc/peer"
+	"net"
+	"strings"
+)
+
+// GetClientIP extracts the client IP address from the context
+func GetClientIP(ctx context.Context) (string, error) {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return "", FailedToGetPeerFromContextError
+	}
+
+	// Get the IP address from the peer address
+	addr := p.Addr.String()
+	ip, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return "", err
+	}
+
+	// Remove any surrounding brackets from IPv6 addresses
+	ip = strings.Trim(ip, "[]")
+
+	return ip, nil
+}
---
+++ context/errors.go
@@ -0,0 +1,9 @@
+package context
+
+import (
+	"errors"
+)
+
+var (
+	FailedToGetPeerFromContextError = errors.New("failed to get peer from context")
+)
