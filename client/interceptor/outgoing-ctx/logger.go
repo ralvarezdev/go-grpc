216986231b566bce58c3099625d7e450ce08d3aa
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-05 15:10:10 -0400

    chore: upgraded dependencies

--- client/interceptor/outgoing-ctx/logger.go
+++ client/interceptor/outgoing-ctx/logger.go
@@ -1,33 +1,30 @@
 package outgoing_ctx
 
 import (
-	gologger "github.com/ralvarezdev/go-logger"
-	gologgerstatus "github.com/ralvarezdev/go-logger/status"
+	gologgermode "github.com/ralvarezdev/go-logger/mode"
+	gologgermodenamed "github.com/ralvarezdev/go-logger/mode/named"
 )
 
 // Logger is the logger for the outgoing context debugger
 type Logger struct {
-	logger gologger.Logger
+	logger gologgermodenamed.Logger
 }
 
 // NewLogger is the logger for the outgoing context debugger
-func NewLogger(logger gologger.Logger) (*Logger, error) {
-	// Check if the logger is nil
-	if logger == nil {
-		return nil, gologger.ErrNilLogger
+func NewLogger(header string, modeLogger gologgermode.Logger) (*Logger, error) {
+	// Initialize the mode named logger
+	namedLogger, err := gologgermodenamed.NewDefaultLogger(header, modeLogger)
+	if err != nil {
+		return nil, err
 	}
 
-	return &Logger{logger: logger}, nil
+	return &Logger{logger: namedLogger}, nil
 }
 
 // LogKeyValue logs the key value
 func (l *Logger) LogKeyValue(key string, value string) {
-	formattedKey := "Outgoing context key '" + key + "' value"
-	l.logger.LogMessage(
-		gologger.NewLogMessage(
-			formattedKey,
-			gologgerstatus.Debug,
-			value,
-		),
+	l.logger.Debug(
+		"outgoing context key '"+key+"' value",
+		value,
 	)
 }
